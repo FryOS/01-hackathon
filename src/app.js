fb277bb6936a0d08bb9041eb0a0a3d7815bd4259
sjivanov <sjivanov@yandex.ru>
2022-06-26 12:34:50 +0300

    Добавление модуля "Вызвать сообщение"

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import './styles.css'
 import {ContextMenu} from './menu';
 import {BackgroundModule} from './modules/background.module';
+import {ShowMessage} from './modules/showMessage.module';
 
 const contextMenu = new ContextMenu('#menu');
 contextMenu.open();
@@ -8,3 +9,6 @@ contextMenu.close();
 
 const backgroundModule = new BackgroundModule('background', 'Изменить цвет фона');
 contextMenu.add(backgroundModule);
+
+const showMessage = new ShowMessage('showmessage', 'Вызвать сообщение');
+contextMenu.add(showMessage);
