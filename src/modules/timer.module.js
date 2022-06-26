a8c4b9e333df884fcc33081c7d54bd471f0646f5
Alexey Ospishchev <alexeyosp@gmail.com>
2022-06-26 13:57:55 +0300

    timer.module рабочая заготовка

--- src/modules/timer.module.js
+++ src/modules/timer.module.js
@@ -9,23 +9,5 @@ export class TimerModule extends Module {
 
         alert("Done!");
 
-        
-        // function randomSound() {
-        //     const arrSound = ['./audio/Rex1.mp3', './audio/Rex2.mp3', './audio/Rex3.mp3', './audio/Rex4.mp3', './audio/Rex5.mp3'];
-        //     const randomSound = Math.floor(Math.random() * arrSound.length);
-        //     return new Audio(arrSound[randomSound]);
-        // }        
-
-        // const promise = randomSound().play();
-        // console.log(promise);
-        // if (promise !== undefined) { // On older browsers play() does not return anything, so the value would be undefined.
-        //   promise
-        //     .then(() => {
-        //         alert("Done!");
-        //     })
-        //     .catch(error => {
-        //       console.log(error);
-        //     });
-        // }
     }
 } 
